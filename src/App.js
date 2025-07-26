7e6f583f25b25d686709a2a8229d62e10e03715f
mariodev99 <lucianomariodev@gmail.com>
2025-07-26 15:28:04 +0200

    add dual

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import "./App.css";
+import React, { useEffect, useState } from "react";
 
 function App() {
+  const [backgroundImage, setBackgroundImage] = useState("");
+
+  useEffect(() => {
+    const images = ["/ticket.jpg", "/ticket-2.jpg"];
+    const randomIndex = Math.floor(Math.random() * images.length);
+    setBackgroundImage(images[randomIndex]);
+  }, []);
+
   return (
-    <div className="container">
-      <div className="pulse-container ">
+    <div
+      className="container"
+      style={{
+        backgroundImage: `url(${backgroundImage})`,
+      }}
+    >
+      <div className="pulse-container">
         <div className="point pulse-effect"></div>
       </div>
     </div>
